fa241a8a81e2e1d8061181a83ef58b5e31cde509
VKumar786 <vkumar@onemindservices.com>
2023-12-02 01:40:54 +0530

    -

--- tut40.py
+++ tut40.py
@@ -1,3 +1,17 @@
 '''
-    TODO:
+    TODO: How import works in Python | Python Tutorial - Day #44 
 '''
+
+import math
+# from math import *        -> not recommended
+from math import sqrt, pi
+import numpy as np
+from numpy import array as arr
+
+math.floor(3.2450)
+
+print(math.sqrt(9), pi)
+
+print(pd.dot([2,32,1,1], [59,54,32,0]))
+
+print(np.array())
